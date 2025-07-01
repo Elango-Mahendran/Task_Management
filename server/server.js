fe2ab339fa317c504aae428135160367c871951f
lokeshvivek2511 <lokeshvlw2004@gmail.com>
2025-07-02 02:49:26 +0530

    `Update server.js to use https://taskmanager-3ybk.onrender.com as the origin in cors middleware`

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your client URL
+  origin: 'https://taskmanager-3ybk.onrender.com', // Replace with your client URL
   credentials: true
 }));
 
